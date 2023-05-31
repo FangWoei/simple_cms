c7c91d5259ff518fe3dc26d6b8dee5a725649b98
Fang_Woei <fangwoei22f@forwardschool.edu.my>
2023-05-31 10:26:31 +0800

    f

--- pages/home.php
+++ pages/home.php
@@ -2,7 +2,7 @@
 
 $database = connectToDB();
 
-  $sql = "SELECT * FROM posts";
+$sql = "SELECT * FROM posts where status = 'publish'";
   $query = $database->prepare($sql);
   $query->execute();
   $posts = $query->fetchAll();
@@ -12,20 +12,23 @@ $database = connectToDB();
 ?>
     <div class="container mx-auto my-5" style="max-width: 500px;">
       <h1 class="h1 mb-4 text-center">My Blog</h1>
-      <?php foreach ($posts as $post) { ?>
-        <?php if ($post['status'] == "publish") {?>
+      <?php foreach ($posts as $post) : ?>
       <div class="card mb-2">
         <div class="card-body">
           <h5 class="card-title"><?= $post['title']; ?></h5>
-          <p class="card-text"><?= $post['content']; ?></p>
+          <p class="card-text">
+          <?php 
+            $excerpt = str_split( $post['content'], 5 );
+            echo $excerpt[0] . "<strong>... read more</strong>"; 
+          ?></p>
+          </p>
           <div class="text-end">
             <a href="/post?id=<?= $post['id']; ?>" class="btn btn-primary btn-sm">Read More</a>
           </div>
-          <?php }?>
         </div>
       </div>
+      <?php endforeach; ?>
 
-      <?php } ?>
       <div class="mt-4 d-flex justify-content-center gap-3">
       <?php if ( isset( $_SESSION["user"] ) ) { ?>
         <a href="/logout" class="btn btn-link btn-sm">Logout</a>
--- pages/post.php
+++ pages/post.php
@@ -1,26 +1,53 @@
 <?php
-if ( isset( $_GET['id'] ) ) {
-  $database = connectToDB();
 
-  $sql = "SELECT * FROM posts WHERE id = :id";
-  $query = $database->prepare( $sql );
-  $query->execute([
-    'id' => $_GET['id']
-  ]);
-  $posts = $query->fetch();
-} else {
-  header("Location: /");
-  exit;
-}
+    // make sure there is an id query string in the url
+    if ( isset( $_GET['id'] ) ) {
 
-  require "parts/header.php";
+        $database = connectToDB();
 
+        // make sure the post is published
+        $sql = "SELECT * FROM posts WHERE id = :id AND status = 'publish'";
+        $query = $database->prepare( $sql );
+        $query->execute([
+            'id' => $_GET['id']
+        ]);
+
+        // fetch
+        $post = $query->fetch();
+
+        if ( !$post ) {
+            // if post don't exists, then we redirect back to home
+            header("Location: /");
+            exit;
+        }
+
+    } else {
+        // if $_GET['id'] is not available, then redirect the user back to home
+        header("Location: /");
+        exit;
+    }
+
+    require "parts/header.php";
 ?>
     <div class="container mx-auto my-5" style="max-width: 500px;">
-      <h1 class="h1 mb-4 text-center"><?= $posts['title']; ?></h1>
-      <p>
-      <?= $posts['content']; ?>
-      </p>
+      <h1 class="h1 mb-4 text-center"><?= $post['title']; ?></h1>
+      <?php 
+      //short
+            echo nl2br( $post['content'] );
+      //long      
+            // // turn this content to an array
+            // $paragraphs_array = preg_split( '/\n\s*\n/', $post["content"] );
+            // // once we have the array, we'll use foreach to print out each line using <p>
+            // foreach( $paragraphs_array as $paragraph ) {
+            //     echo "<p>";
+            //     // split by breakline
+            //     $lines_array = preg_split( '/\n/',$paragraph );
+            //     foreach( $lines_array as $line ) {
+            //         echo "$line<br />";
+            //     }
+            //     echo "</p>";
+            // }
+        ?>
       <div class="text-center mt-3">
         <a href="/dashboard" class="btn btn-link btn-sm"
           ><i class="bi bi-arrow-left"></i> Back</a
